60af9f6b42ace9c7749bc8bfa9a6bb73b29c8e21
DedierJr <dedierjr2005@gmail.com>
2023-04-20 16:47:13 -0300

    imc - (incompleto)

---
+++ exercicioIMC/App.js
@@ -0,0 +1,54 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Speech from "expo-speech";
+
+export default function App() {
+  const [valor1, setValor1] = useState("");
+  const [valor2, setValor2] = useState("");
+  let resultado = 0;
+
+  const somar = () => {
+    resultado = parseFloat(valor1) / (parseFloat(valor2) * parseFloat(valor2));
+    Speech.speak("Seu IMC é: " + resultado);
+  };
+}
+
+return (
+  <View style={styles.container}>
+    <TextInput
+      placeholder="Digite Valor 1"
+      keyboardType="numeric"
+      onChangeText={(valor1) => setValor1(valor1)}
+      value={valor1}
+      style={styles.input}
+    ></TextInput>
+
+    <TextInput
+      placeholder="Digite Valor 2"
+      keyboardType="numeric"
+      onChangeText={(valor2) => setValor2(valor2)}
+      value={valor2}
+      style={styles.input}
+    ></TextInput>
+
+    <Button onPress={somar} title="Pressione para somar"></Button>
+    <StatusBar style="auto" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "grey",
+    borderRadius: 15,
+    marginTop: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+});
